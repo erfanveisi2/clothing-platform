632c1f493e3d585f9a3a36d414439102be93bb22
erfan <erfanveisi2@gmail.com>
2021-12-24 15:06:39 +0300

    navigate between pages withRouter

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import './directory.styles.scss'
 import MenuItem from "../menu-item/menu-itme.component";
 
-
 class Directory extends React.Component{
     constructor() {
         super();
@@ -52,8 +51,8 @@ render() {
         return(
             <div className='directory-menu'>
                 {
-                    this.state.sections.map(({title , imageUrl , id, size})=>(
-                        <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>
+                    this.state.sections.map(({id, ...otherSectionProps})=>(
+                        <MenuItem key={id} {...otherSectionProps}/>
                     ))
                 }
             </div>
