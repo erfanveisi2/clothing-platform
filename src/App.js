82f22fdf4074347e185c8e3657d6bf2ca3466be3
erfan <erfanveisi2@gmail.com>
2021-12-25 16:42:29 +0300

    add header and finish styling

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import {
 import './App.css';
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
 
 
 
@@ -15,8 +16,11 @@ import ShopPage from "./pages/shop/shop.component";
 function App() {
   return (
     <div >
-        <Route exact path='/'  component={HomePage}/>
-        <Route path='/shop' component={ShopPage}/>
+        <Header/>
+        <Switch>
+            <Route exact path='/'  component={HomePage}/>
+            <Route path='/shop' component={ShopPage}/>
+        </Switch>
     </div>
   );
 }
